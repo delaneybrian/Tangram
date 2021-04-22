719a43c9acfaeb221919d3c20fc48a94816bc3bd
Brian Delaney <delaneybrian6@gmail.com>
2021-04-22 20:48:24 +0100

    Finished Mongo

--- src/Tangram.Mongo/MongoEventStore.cs
+++ src/Tangram.Mongo/MongoEventStore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using Tangram.Exceptions;
 
 namespace Tangram.Mongo
 {
@@ -37,11 +38,39 @@ namespace Tangram.Mongo
 
         public override IEnumerable<IEvent> Save<TAggregate>(TAggregate aggregate)
         {
+            var mongoClient = _connectionFactory.CreateConnection();
+
+            return SaveEvents(mongoClient, aggregate);
         }
 
         public override IEnumerable<IEvent> Save<TAggregate, TSnapshot>(TAggregate aggregate)
         {
+            var mongoClient = _connectionFactory.CreateConnection();
 
+            var evts = SaveEvents(mongoClient, aggregate).ToList();
+
+            if (_snapshottingEnabled && SnapshotRequired(evts, _snapshotFrequency))
+            {
+                var snapshot = aggregate.ToSnapshot();
+                var json = _serializer.Serialize(snapshot);
+
+                var database = mongoClient.GetDatabase(_mongoEventStoreConfiguration.DatabaseName);
+
+                var snapshotsCollection =
+                    database.GetCollection<MongoEntity>(_mongoEventStoreConfiguration.SnapshotsCollectionName);
+
+                var mongoEntity = new MongoEntity
+                {
+                    Id = aggregate.Id,
+                    Version = snapshot.Version,
+                    Payload = _serializer.Serialize(snapshot)
+                };
+
+                snapshotsCollection
+                    .InsertOne(mongoEntity);
+            }
+
+            return evts;
         }
 
         public override TAggregate GetById<TAggregate>(Guid id)
@@ -64,29 +93,79 @@ namespace Tangram.Mongo
             var snapshotsCollection = database.GetCollection<MongoEntity>(_mongoEventStoreConfiguration.SnapshotsCollectionName);
 
             Func<IAggregate> makeAggregate = () =>
-                {
-                    var aggregate = new TAggregate();
+            {
+                var aggregate = new TAggregate();
 
-                    if (!_snapshottingEnabled)
-                        return aggregate;
-
-                    var latestSnapshot = snapshotsCollection.AsQueryable()
-                        .OrderBy(x => x.Version)
-                        .FirstOrDefault();
-
-                    if (latestSnapshot == null)
-                        return aggregate;
-
-                    var snapshot = (TSnapshot)_serializer.Deserialize<IAggregateSnapshot>(latestSnapshot.Payload.ToString());
-
-                    aggregate.RestoreFromSnapshot(snapshot);
-
+                if (!_snapshottingEnabled)
                     return aggregate;
-                };
+
+                var latestSnapshot = snapshotsCollection.AsQueryable()
+                    .OrderBy(x => x.Version)
+                    .FirstOrDefault();
+
+                if (latestSnapshot == null)
+                    return aggregate;
+
+                var snapshot = (TSnapshot)_serializer.Deserialize<IAggregateSnapshot>(latestSnapshot.Payload.ToString());
+
+                aggregate.RestoreFromSnapshot(snapshot);
+
+                return aggregate;
+            };
 
             var eventsCollection = database.GetCollection<MongoEntity>(_mongoEventStoreConfiguration.EventsCollectionName);
 
             return (TAggregate)GetById(eventsCollection, id, typeof(TAggregate), makeAggregate);
+        }
+
+        private IEnumerable<IEvent> SaveEvents(IMongoClient mongoClient, IAggregate aggregate)
+        {
+            if (!aggregate.UncommitedEvents().Any())
+            {
+                return new List<IEvent>();
+            }
+
+            var evtsToSave = aggregate.UncommitedEvents().ToList();
+            var expected = CalculateExpectedVersion(aggregate, evtsToSave);
+
+            var database = mongoClient.GetDatabase(_mongoEventStoreConfiguration.DatabaseName);
+
+            var eventsCollection = database.GetCollection<MongoEntity>(_mongoEventStoreConfiguration.EventsCollectionName);
+
+            using (var session = mongoClient.StartSession())
+            {
+                try
+                {
+                    var current = eventsCollection
+                        .AsQueryable()
+                        .Count(x => x.Id == aggregate.Id) - 1;
+
+                    if (expected != current)
+                    {
+                        throw new AggregateConflictException(aggregate.Id, expected, current);
+                    }
+
+                    var mongoEventsToSave = evtsToSave.Select(e => new MongoEntity
+                    {
+                        Id = e.AggregateId,
+                        Version = e.Version,
+                        Payload = _serializer.Serialize(e)
+                    });
+
+                    eventsCollection
+                            .InsertMany(session, mongoEventsToSave);
+
+                    session.CommitTransaction();
+                }
+                catch (Exception)
+                {
+                    session.AbortTransaction();
+
+                    throw;
+                }
+            }
+
+            return aggregate.UncommitedEvents();
         }
 
         private IAggregate GetById(IMongoCollection<MongoEntity> collection, Guid id, Type type, Func<IAggregate> aggregateFunc)
