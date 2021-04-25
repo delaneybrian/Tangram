5e91339b168de0e55711731b9d8d1f610b98d776
Brian Delaney <delaneybrian6@gmail.com>
2021-04-25 18:34:45 +0100

    conflict resolution

--- src/MongoSample/Program.cs
+++ src/MongoSample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Autofac;
 using Tangram;
+using Tangram.Exceptions;
 
 namespace MongoSample
 {
@@ -14,28 +16,93 @@ namespace MongoSample
             {
                 var eventStore = scope.Resolve<IEventStore>();
 
-                var person = new Person(
-                    18,
-                    "Brian",
-                    "Delaney", new
-                        DateTime(2020, 04, 22),
-                    Guid.NewGuid());
+                //var person = new Person(
+                //    18,
+                //    "Brian",
+                //    "Delaney", new
+                //        DateTime(2020, 04, 22),
+                //    Guid.NewGuid());
 
-                person.UpdateAge(23, DateTime.UtcNow.AddHours(1), Guid.NewGuid());
+                //person.UpdateAge(23, DateTime.UtcNow.AddHours(1), Guid.NewGuid());
 
-                person.UpdateName("Jimmy", "Delaney", DateTime.UtcNow.AddHours(4), Guid.NewGuid());
+                //person.UpdateName("Jimmy", "Delaney", DateTime.UtcNow.AddHours(4), Guid.NewGuid());
 
-                eventStore.Save<Person, PersonSnapshot>(person);
+                //eventStore.Save<Person, PersonSnapshot>(person);
 
-                UpdateAge(scope, person.Id);
+                //UpdateAge(scope, person.Id);
 
-                UpdateName(scope, person.Id);
+                //UpdateName(scope, person.Id);
 
-                UpdateAge(scope, person.Id);
+                //UpdateAge(scope, person.Id);
+
+                var id = Guid.Parse("d2c4feb1-913b-4606-a649-38bf6c90ddf4");
+
+                BackgroundUpdate(id);
+
+                NonBackgroundUpdate(id).Wait();
+
             }
 
             Console.WriteLine("Finished");
             Console.ReadKey();
+        }
+
+        public static async Task BackgroundUpdate(Guid id)
+        {
+            var container = Bootstrapper.Bootstrap();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var eventStore = scope.Resolve<IEventStore>();
+
+                try
+                {
+                    var person = eventStore.GetById<Person, PersonSnapshot>(id);
+
+                    person.UpdateAge(55, DateTime.UtcNow.AddHours(1), Guid.NewGuid());
+
+                    await Task.Delay(TimeSpan.FromSeconds(30));
+
+                    eventStore.Save<Person, PersonSnapshot>(person);
+                }
+                catch (AggregateConflictException ex)
+                {
+                    var p = eventStore.GetById<Person, PersonSnapshot>(id);
+
+                    p.UpdateAge(55, DateTime.UtcNow.AddHours(1), Guid.NewGuid());
+
+                    eventStore.Save<Person, PersonSnapshot>(p);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        public static async Task NonBackgroundUpdate(Guid id)
+        {
+            var container = Bootstrapper.Bootstrap();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                try
+                {
+                    var eventStore = scope.Resolve<IEventStore>();
+
+                    var person = eventStore.GetById<Person, PersonSnapshot>(id);
+
+                    person.UpdateAge(66, DateTime.UtcNow.AddHours(1), Guid.NewGuid());
+
+                    eventStore.Save<Person, PersonSnapshot>(person);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
         }
 
         private static void UpdateAge(ILifetimeScope scope, Guid id)
